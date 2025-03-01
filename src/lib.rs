6b615054e831c3d595798a675777bccc910bbef3
mahiro113 <mahiromhf@gmail.com>
2025-03-01 16:52:23 +0900

    lib.rsにCli構造体を分離

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@ use clap::Parser;
 
 #[derive(Debug,Parser)]
 pub struct Cli {
-    #[arg(short,long,help="GeminiのAPIKEYをわたしてください")]
-    key: String,
+    #[arg(short,long,required=true,help="GeminiのAPIKEYをわたしてください")]
+    pub key: String,
 
-    #[arg(short,long,help="Geminiと会話する内容を入力してください")]
-    text: String,
+    #[arg(short,long,required=true,help="Geminiと会話する内容を入力してください")]
+    pub text: String,
 
     #[arg(short,long,action=clap::ArgAction::SetFalse,help="対話形式で話したい場合は引数に追加してください")]
-    chat: bool
+    pub chat: bool
 } 
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 use clap::Parser;
-
-#[derive(Debug,Parser)]
-pub struct Cli {
-    #[arg(short,long,help="GeminiのAPIKEYをわたしてください")]
-    key: String,
-
-    #[arg(short,long,help="Geminiと会話する内容を入力してください")]
-    text: String,
-
-    #[arg(short,long,action=clap::ArgAction::SetFalse,help="対話形式で話したい場合は引数に追加してください")]
-    chat: bool
-} 
+use ttalk::Cli;
 
 fn main() {
     let args = Cli::parse();
-    println!("pattern: {:?}, path: {:?}, caht: {:?}", &args.key, &args.text,args.chat)
+    println!("pattern: {:?}, path: {:?}, chat: {:?}", &args.key, &args.text,args.chat)
 }
